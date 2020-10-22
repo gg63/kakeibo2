afcdf19dabd8c7fbde038fd531ada98bec344eaf
gg <wakadanna0603@gmail.com>
2020-10-22 18:28:42 +0000

    newの開発,careateの開発

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,4 +7,16 @@ class BooksController < ApplicationController
     def show
         @book = Book.find(params[:id])
     end 
+    
+    def new
+        @book = Book.new
+        @book.year = 2019
+    end 
+    
+    def create
+        book_params = params.require(:book).permit(:year, :month, :inout, :category, :amount)
+        @book = Book.new(book_params)
+        @book.save
+        redirect_to books_path
+    end 
 end 
